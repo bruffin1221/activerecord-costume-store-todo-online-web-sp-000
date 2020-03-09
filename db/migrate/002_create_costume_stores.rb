c393de219975f80e6878dafe0db6a5f0ed0f7e5b
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-09 20:41:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,8 @@ def change
   create_table :costume_stores do |t|
     t.string :name
     t.string :location
-    t.integer :number_of_costumes
-    t.integer :number_of_employees
+    t.integer :costume_inventory
+    t.integer :employees_count
     t.boolean :still_in_business
     t.datetime :opening_time
     t.datetime :closing_time
